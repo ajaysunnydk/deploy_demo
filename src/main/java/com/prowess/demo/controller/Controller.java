39852cc4500e87a262f1fc78202e3d60c0032754
ajaysunnydk <ajaysunnydk167@gmail.com>
2023-02-05 11:18:53 +0530

    Added Files

---
+++ src/main/java/com/prowess/demo/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.prowess.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping
+	public String test() {
+		return "Test success";
+	}
+	
+}
